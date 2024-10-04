dccd659931cad1f142c546a00ad4a0f47747db02
IgorMontezuma20 <igorminranda17@gmail.com>
2024-10-04 01:43:57 -0300

    feat: Add resume component

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import  VacancyListComponent  from '../../components/vacancy-list/vacancy-list.component';
+import { ResumeComponent } from '../../components/resume/resume.component';
+import VacancyListComponent from '../../components/vacancy-list/vacancy-list.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, VacancyListComponent],
+  imports: [CommonModule, VacancyListComponent, ResumeComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
